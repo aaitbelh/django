d4106b5ffa460954257ca24aaad434d1e9077d48
aaitbelh <aitbelhajayoub566@gmail.com>
2024-10-03 15:10:12 +0100

    authentication with django

--- mysite/playground/models.py
+++ mysite/playground/models.py
@@ -6,14 +6,3 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
-    username = None  # Remove the username field
-
-    USERNAME_FIELD = 'email'  # Use email for authentication
-    REQUIRED_FIELDS = []  # No other fields required for superuser
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
--- mysite/playground/views.py
+++ mysite/playground/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 # Create your views here.
 from . import userauth
 
@@ -12,9 +15,11 @@ def login_page(request):
 
 @csrf_exempt
 def say_hello_To(request):
-    print("Hii")
-    userauth.authenticateUser()
-    return HttpResponse("hello")
+	if request.user.is_authenticated :
+		return HttpResponse(f"hello {request.user}")
+	else:
+		messages.info(request, "you are not logged yet")
+		return redirect('/')
 
 def home_page(request):
     return HttpResponse("homepage")
@@ -22,12 +27,13 @@ def home_page(request):
 @csrf_exempt
 def login_user(request):
 	if request.method == 'POST':
-		print(request)	
-		# username = request.POST['USER']
-		# password = request.POST['password']
-		# user = authenticate(username="john", password="secret")
-		# if user is not None:
-		# 	login(request, user)
-		# 	return redirect('hello/')
-		# else:
-		# 	return render(request, 'hello.html')
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('/hello/')
+		else:
+			us = User.objects.create_user(username, "lennon@thebeatles.com", password)
+			us.save()
+			return redirect('/')
